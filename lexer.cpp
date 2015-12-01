18ab78a140721cb005f085bb708d0b585dbba14f
Marcus Godwin <marcusgodwin@hotmail.com>
2015-12-01 16:06:48 -0500

    Lexer mostly implemented.
    Use C++11 regexes. Added an array of regexes. Does not handle comments.

--- lexer.cpp
+++ lexer.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <regex>
 #include <ctype.h>
 #include "lexer.h"
 
@@ -10,24 +11,59 @@ LexerTarget::LexerTarget(std::string name) {
     filename = name;
     content = read_file(name);
     lineNum = 0;
+    colNum = 0;
+    sub_begin = sub_len = 0;
 }
 
 LexerTarget::~LexerTarget() {
 }
 
 std::string LexerTarget::lex() {
-    std::cout << "Lexing!!!!!!\n";
 
     std::string nonalpha = "{}[]()<>*+-/=.==\"";
     std::string two_char = "/+=-*";
     std::string token = "";
-    int comment_depth = 0;
+    int comment_depth;
     char last_char = 0;
+
+    std::string ln = content.at(lineNum);
     
+    int i;
+
+    if(comment_depth) {
+        return lexcomment();
+    }
+    
+    while(isspace(ln[sub_begin])) {
+        sub_begin += 1;
+        if(ln.size() <= sub_begin) {
+            sub_begin = 0;
+            lineNum++;
+            ln = content.at(lineNum);
+        }
+    }
+
+    char c = ln[sub_begin];
+
+    if(nonalpha.find(c,0) != std::string::npos) {
+        if(ln.size() > sub_begin+1) {
+            if(c == '/' && ln[sub_begin+1] == '*') {
+                std::cout << "comment begin \n";
+                sub_begin += 1;
+                comment_depth++;
+            }
+        }
+    }
+
+    for(i = sub_begin; i < ln.size(); i++,sub_len++) {
+        char c = ln[i];
+
+    }
+
     for (auto st : content) {
         std::cout << "\n" << lineNum << ": " << st << "\n";
-        int sub_begin = 0;
-        int sub_len = 0;
+        sub_begin = 0;
+        sub_len = 0;
         int count = 0;
         for(auto& c : st) {
             count++;
@@ -108,12 +144,88 @@ std::string LexerTarget::lex() {
     }
 }
 
+std::string LexerTarget::lexcomment() {
+
+    return "";
+}
+
+std::string LexerTarget::lex(bool b) {
+    std::string token = "";
+    std::string tokentype = "";
+
+    if(lineNum >= content.size()) {
+        return "EOF";
+    }
+    
+    std::string ln = content.at(lineNum);
+    if(colNum >= ln.size()) {
+        lineNum++;
+        colNum = 0;
+        if(lineNum >= content.size())
+            return "EOF";
+        ln = content.at(lineNum);
+    }
+
+    while(isspace(ln[colNum])) {
+        colNum++;
+
+        //std::cout << "there is a space!!!\n";
+        if(colNum >= ln.size()) {
+            if(lineNum+1 >= content.size()) {
+                return "EOF";
+            } else {
+                lineNum++;
+                ln = content.at(lineNum);
+                colNum = 0;
+            }
+        }
+    }
+            
+
+    std::regex longest_regex_match;
+    int longest_match = 0;
+
+    //std::cout << "colNum = " << colNum << '\n';
+    std::cout << ln << '\n';
+    for(int c = 0; c < colNum-1; c++) {
+        std::cout << '-';
+    }
+    std::cout << "-^\n";
+    for (int i = 0; i < num_regexes; i++) {
+        std::smatch tmp;
+        std::string remaining = ln.substr(colNum);
+        bool matched = std::regex_search(remaining,tmp,regexes[i]);
+        if(matched) {
+            //std::cout << "Matched for regex: " << i << '\n';
+            //std::cout << "Total # of matches: " << tmp.size() << '\n';
+        for(int j = 0; j < tmp.size(); j++) {
+            //make sure match starts at beginning of last token
+            //std::cout << "Match for: " << tmp[j] << " at pos " << tmp.position(j) <<'\n';
+            if(tmp.position(j) != 0) continue;
+
+            if(tmp.length(j) > longest_match) {
+                longest_match = tmp.length(j);
+                longest_regex_match = regexes[i];
+            }
+        }
+        }
+    }
+    
+    token = ln.substr(colNum,longest_match);
+    colNum += longest_match;
+    
+    std::cout << "token: " << token << "\n\n";
+    return token;
+
+}
+
 std::vector<std::string> read_file(const std::string& filename) {
     std::ifstream in(filename);
     std::vector<std::string> content;
     std::string line;
     while(std::getline(in, line)) {
         content.push_back(line);
+        std::cout << line << '\n';
     }
     
     return content;
@@ -121,6 +233,10 @@ std::vector<std::string> read_file(const std::string& filename) {
 
 int main() {
     LexerTarget target1 = LexerTarget("test.txt");
-    target1.lex();
+    //target1.lex();
+    std::string tok;
+    while(tok.compare("EOF") != 0) {
+        tok = target1.lex(true);
+    }
     return 0;
 }
--- lexer.h
+++ lexer.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <regex>
 
 class LexerTarget
 {
@@ -10,15 +11,46 @@ public:
     LexerTarget(std::string name);
     ~LexerTarget();
     std::string lex();
+    std::string lex(bool);
+    std::string lexcomment();
 
 private:
     std::string filename;
     std::vector<std::string> content;
-    int lineNum;
-    //int sub_begin, sub_pos;
+    int lineNum, colNum;
+    int sub_begin, sub_len;
     /* data */
 };
 
 std::vector<std::string> read_file(const std::string& filename);
 
+std::regex regexes[] = { 
+    std::regex("fn"),
+    std::regex("extern"),
+    std::regex("[a-zA-Z_][a-zA-Z0-9_]*"),
+    std::regex("[0-9]+\\.[0-9]*"),
+    std::regex("[0-9]+"),
+    std::regex("="),    
+    std::regex("=="),    
+    std::regex("!="),    
+    std::regex("<"),    
+    std::regex("<="),    
+    std::regex(">"),    
+    std::regex(">="),    
+    std::regex("\\("),    
+    std::regex("\\)"),    
+    std::regex("\\{"),    
+    std::regex("\\}"),    
+    std::regex("\\."),    
+    std::regex("\\,"),    
+    std::regex("\\+"),    
+    std::regex("-"),    
+    std::regex("\\*"),    
+    std::regex("/"),    
+    std::regex(";"),    
+    std::regex(":"),    
+    std::regex("!"),  
+    std::regex("\\?")   
+};
+int num_regexes = sizeof(regexes)/sizeof(std::regex);
 #endif
